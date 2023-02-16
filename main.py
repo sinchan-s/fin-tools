818bb6237303ae51f23c1b4f2cf85869ec9e77e0
Sinchan <sinchan.tex@gmail.com>
2023-02-16 22:13:05 +0530

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+#! Important Libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+
+#! basic configurations
+st.set_page_config(
+    page_title="Fin-Tools",
+    page_icon="💸",
+    layout="wide",
+    initial_sidebar_state="collapsed")
+
+#! App header
+st.header("Welcome to the solutions everything related to money")
