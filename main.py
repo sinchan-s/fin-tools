27d65c4ea3fb51dc2fa787c20f1a3b4d69f30ff8
Sinchan <sinchan.tex@gmail.com>
2023-02-17 23:46:15 +0530

    testing tabs

--- main.py
+++ main.py
@@ -13,4 +13,15 @@ st.set_page_config(
 #! App header
 st.header("Welcome to the solutions everything related to money")
 
-st.button('Calculate Home Loan EMIs')
+def hl_emi():
+    st.balloons()
+
+def sip():
+    st.snow()
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Home Loan','SIP','NPS','Tab4','Dividends'])
+tab1.button('Calculate Home Loan EMIs', on_click=hl_emi)
+tab2.button('Calculate SIP returns', on_click=sip)
+tab3.button('Calculate NPS returns', on_click=hl_emi)
+tab4.button('Calculate  returns', on_click=sip)
+tab5.button('Calculate dividends', on_click=hl_emi)
