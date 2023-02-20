dd4d8dd8cb454ec9d3ef1f3e667c408a3e580fb8
Sinchan <sinchan.tex@gmail.com>
2023-02-20 23:23:55 +0530

    sip tab modified

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import seaborn as sns
+import numpy as np
 
 #! Basic configurations
 st.set_page_config(
@@ -19,18 +21,24 @@ def hl_emi():
 def sip():
     pass
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Home Loan','SIP','NPS','Tab4','Dividends'])
-tab1.button('Calculate Home Loan EMIs', on_click=hl_emi)
+tab1, tab2 = st.tabs(['SIP', 'HL'])
 
-sip_amt = tab2.slider('Monthly Investment',500,100000)
-interest_r = tab2.slider('Expected Return (p.a.)',0.1,30.0)
-time_period = tab2.slider('Time Period',1,40)
-
+t1col1, t1col2 = tab1.columns(2)
+sip_amt = t1col1.select_slider('Monthly Investment',[n for n in range(500,100000,500)])
+interest_r = t1col1.select_slider('Expected Return (p.a.)',[round(i,1) for i in np.arange(0.5,30.1,0.1)])
+time_period = t1col1.slider('Time Period',1,40)
 returns_t = sip_amt*(((1 + interest_r/1200)**(time_period*12)-1)/(interest_r/1200))*(1+interest_r/1200)
-tab2.metric('Invested',sip_amt*time_period*12)
-tab2.metric('Returns',round(returns_t-sip_amt*time_period*12))
-tab2.metric('Total Value',round(returns_t))
+invested = sip_amt*time_period*12
+exp_return = round(returns_t-invested)
+# pie_data = {'Exp. Returns': exp_return, 'Invested': invested}
+# st.write(pie_data.values())
+# st.write(pie_data.keys())
+# st.write(pie_data.index(name, value))
+# palette_color = sns.color_palette('bright')
+# fig = plt.figure(figsize=(6,6))
+# plt.pie(data=pie_data.values(), labels=pie_data.keys(), colors=palette_color, autopct='%.0f%%')
+# st.pyplot(fig)
 
-tab3.button('Calculate NPS returns', on_click=hl_emi)
-tab4.button('Calculate  returns', on_click=sip)
-tab5.button('Calculate dividends', on_click=hl_emi)
+t1col2.metric('Total Invested', invested,)
+t1col2.metric('Returns', exp_return,f'{round(exp_return*100/invested,2)}%')
+t1col2.metric('Total Value', round(returns_t),f'{round(returns_t/invested,2)}x ')
